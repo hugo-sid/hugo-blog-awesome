cfa6ca6b5b41bc4f0138787bc1a9a4ef89d942be
Vladislav <24293461+kusyka911@users.noreply.github.com>
2023-04-28 18:20:35 +0300

    feat: add automatic dark and light mode detection (#26)
    Co-authored-by: Sid <122173059+hugo-sid@users.noreply.github.com>

--- assets/js/themeLoader.js
+++ assets/js/themeLoader.js
@@ -1,16 +1,38 @@
 const body = document.body;
-const data = body.getAttribute("data-theme");
+const config = body.getAttribute("data-theme");
 
-const initTheme = (state) => {
-  if (state === "dark") {
-    body.setAttribute("data-theme", "dark");
-  } else if (state === "light") {
-    body.removeAttribute("data-theme");
-  } else {
-    localStorage.setItem("theme", data);
-  }
+const getThemeState = () => {
+    const lsState = localStorage.getItem("theme");
+    if (lsState) return lsState;
+
+    let state;
+    switch (config) {
+        case "dark":
+            state = "dark";
+            break;
+        case "light":
+            state = "light";
+            break;
+        case "auto":
+        default:
+            state = window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "dark"
+                : "light";
+            break;
+    }
+    return state;
 };
 
-initTheme(localStorage.getItem("theme"));
+const initTheme = (state) => {
+    if (state === "dark") {
+        document.documentElement.classList.add("dark");
+        document.documentElement.classList.remove("light");
+    } else if (state === "light") {
+        document.documentElement.classList.remove("dark");
+        document.documentElement.classList.add("light");
+    }
+};
+
+initTheme(getThemeState());
 
 setTimeout(() => body.classList.remove("notransition"), 75);
--- assets/js/themeSwitchnMenu.js
+++ assets/js/themeSwitchnMenu.js
@@ -1,32 +1,26 @@
 (() => {
     // Theme switch
-    const body = document.body;
     const lamp = document.getElementById("mode");
-  
+
     const toggleTheme = (state) => {
-      if (state === "dark") {
-        localStorage.setItem("theme", "light");
-        body.removeAttribute("data-theme");
-      } else if (state === "light") {
-        localStorage.setItem("theme", "dark");
-        body.setAttribute("data-theme", "dark");
-      } else {
-        initTheme(state);
-      }
+        if (state === "dark") {
+            localStorage.setItem("theme", "light");
+            initTheme("light");
+        } else if (state === "light") {
+            localStorage.setItem("theme", "dark");
+            initTheme("dark");
+        }
     };
-  
-    lamp.addEventListener("click", () =>
-      toggleTheme(localStorage.getItem("theme"))
-    );
-  
+
+    lamp.addEventListener("click", () => toggleTheme(getThemeState()));
+
     // Blur the content when the menu is open
     const cbox = document.getElementById("menu-trigger");
-  
+
     cbox.addEventListener("change", function () {
-      const area = document.querySelector(".wrapper");
-      this.checked
-        ? area.classList.add("blurry")
-        : area.classList.remove("blurry");
+        const area = document.querySelector(".wrapper");
+        this.checked
+            ? area.classList.add("blurry")
+            : area.classList.remove("blurry");
     });
-  })();
-  
+})();
