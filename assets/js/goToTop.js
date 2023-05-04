6ab4eafe80a844c975138ce20becda7497be0491
Vladyslav Shurbin <2917600-kusyka911@users.noreply.gitlab.com>
2023-05-04 18:54:19 -0400

    fix: refactor js code
    (cherry picked from commit 5163fd1532995d81b22bbcc908629b28111a9e71)

--- assets/js/goToTop.js
+++ assets/js/goToTop.js
@@ -1,12 +1,16 @@
-const gttButton = document.getElementById("totop");
-
-window.onscroll = () =>
-{
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    gttButton.style.visibility = "visible";
-    gttButton.style.opacity = "1";
-  } else {
-    gttButton.style.visibility = "hidden";
-    gttButton.style.opacity = "0";
-  }
-}
+window.addEventListener('load', () => {
+    const gttButton = document.getElementById("totop");
+    if (!gttButton) return;
+    window.onscroll = () => {
+        if (
+            document.body.scrollTop > 300 ||
+            document.documentElement.scrollTop > 300
+        ) {
+            gttButton.style.visibility = "visible";
+            gttButton.style.opacity = "1";
+        } else {
+            gttButton.style.visibility = "hidden";
+            gttButton.style.opacity = "0";
+        }
+    };
+});
