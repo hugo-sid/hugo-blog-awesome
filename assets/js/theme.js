9d9f2bc46589f017dbfca714380bcc017de5ea49
Vladislav <24293461+kusyka911@users.noreply.github.com>
2023-05-21 14:56:23 +0300

    refactor: refactor JS (#39)
    * fix: refactor js code
    
    (cherry picked from commit 5163fd1532995d81b22bbcc908629b28111a9e71)
    
    * docs: refactor docs regarding addition of scripts
    
    * chore: remove `| fingerprint` in development environment
    
    ---------
    
    Co-authored-by: Vladyslav Shurbin <24293461+kusyka911@users.noreply.github.com>
    Co-authored-by: Sid <122173059+hugo-sid@users.noreply.github.com>

---
+++ assets/js/theme.js
@@ -0,0 +1,77 @@
+(() => {
+    "use strict";
+    const LS_THEME_KEY = "theme";
+    const THEMES = {
+        LIGHT: "light",
+        DARK: "dark",
+        AUTO: "auto",
+    };
+
+    const body = document.body;
+    const config = body.getAttribute("data-theme");
+
+    const getThemeState = () => {
+        const lsState = localStorage.getItem(LS_THEME_KEY);
+        if (lsState) return lsState;
+
+        let state;
+        switch (config) {
+            case THEMES.DARK:
+                state = THEMES.DARK;
+                break;
+            case THEMES.LIGHT:
+                state = THEMES.LIGHT;
+                break;
+            case THEMES.AUTO:
+            default:
+                state = window.matchMedia("(prefers-color-scheme: dark)")
+                    .matches
+                    ? THEMES.DARK
+                    : THEMES.LIGHT;
+                break;
+        }
+        return state;
+    };
+
+    const initTheme = (state) => {
+        if (state === THEMES.DARK) {
+            document.documentElement.classList.add(THEMES.DARK);
+            document.documentElement.classList.remove(THEMES.LIGHT);
+        } else if (state === THEMES.LIGHT) {
+            document.documentElement.classList.remove(THEMES.DARK);
+            document.documentElement.classList.add(THEMES.LIGHT);
+        }
+    };
+
+    // init theme ASAP, then do the rest.
+    initTheme(getThemeState());
+    requestAnimationFrame(() => body.classList.remove("notransition"))
+    setTimeout(() => {
+        const toggleTheme = () => {
+            const state = getThemeState();
+            if (state === THEMES.DARK) {
+                localStorage.setItem(LS_THEME_KEY, THEMES.LIGHT);
+                initTheme(THEMES.LIGHT);
+            } else if (state === THEMES.LIGHT) {
+                localStorage.setItem(LS_THEME_KEY, THEMES.DARK);
+                initTheme(THEMES.DARK);
+            }
+        };
+
+        window.addEventListener("DOMContentLoaded", () => {
+            // Theme switch
+            const lamp = document.getElementById("mode");
+
+            lamp.addEventListener("click", () => toggleTheme());
+
+            // Blur the content when the menu is open
+            const cbox = document.getElementById("menu-trigger");
+
+            cbox.addEventListener("change", function () {
+                const area = document.querySelector(".wrapper");
+                if (this.checked) return area.classList.add("blurry");
+                area.classList.remove("blurry");
+            });
+        });
+    }, 0)
+})();
